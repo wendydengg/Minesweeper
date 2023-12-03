4a6ee4d0a3d4ee68b79a5198826e32cae81f650d
haoyund <haoyund@sas.upenn.edu>
2023-12-03 15:24:16 -0500

    add comments

--- Cell.hpp
+++ Cell.hpp
@@ -1,6 +1,7 @@
 //possible states:
-//unopened tiles
-//numbered tiles
-//blank tiles
-//flagged tiles
-//(?) Question marked tiles
+//1. unopened tiles: blank and clickable
+//2. opened tiles: exposed and unclickable
+//2.1 numbered tiles
+//2.2 blank tiles
+//3. flagged tiles: unopened cells marked by the player to indicate a potential mine location, unclickable
+//4. (?) Question marked tiles
---
+++ Game.hpp
@@ -0,0 +1,5 @@
+//1. first click is always safe (lands on number or blank)
+//2. if clicked on bomb, game ends
+//2.a when game ends, player cant do any further move, only restart button is clickable
+//3. else, if clicked on number or a blank tile, all adjacent cells will automatically be opened
+//3.a chain reaction: any blank tiles opened by other blank tiles open the surrounding tiles too
