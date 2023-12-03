431764fcce1d113f3eeaf948332001e1f19d7c0a
haoyund <haoyund@sas.upenn.edu>
2023-12-03 17:28:56 -0500

    first implementation

--- Board.hpp
+++ Board.hpp
@@ -1,4 +1,25 @@
-//#include "Vector2D.hpp"
+// Board.hpp
+#ifndef BOARD_HPP
+#define BOARD_HPP
+
+#include "Cell.hpp"
+#include <vector>
+#include <random>
+
+class Board {
+    std::vector<std::vector<Cell>> grid;
+    int width, height;
+
+public:
+    Board(int width, int height) : width(width), height(height), grid(width, std::vector<Cell>(height)) {}
+
+    void placeMines(int mineCount);
+    void revealCell(int x, int y);
+    void toggleFlag(int x, int y);
+};
+
+#endif 
+
 //2d array 
 //each cell is a Cell class
 //3 levels:
--- Cell.hpp
+++ Cell.hpp
@@ -1,7 +1,45 @@
-//possible states:
-//1. unopened tiles: blank and clickable
+// Cell.hpp
+#ifndef CELL_HPP
+#define CELL_HPP
+
+class Cell {
+public:
+    enum class State {
+        Unopened,
+        Opened,
+        Flagged,
+        Questioned
+    };
+
+    bool isMine;
+    State state;
+    int adjacentMines;
+
+    Cell() : isMine(false), state(State::Unopened), adjacentMines(0) {}
+
+    void open() {
+        if (state == State::Unopened || state == State::Questioned) {
+            state = State::Opened;
+        }
+    }
+
+    void toggleFlag() {
+        if (state == State::Unopened) {
+            state = State::Flagged;
+        } else if (state == State::Flagged) {
+            state = State::Questioned;
+        } else if (state == State::Questioned) {
+            state = State::Unopened;
+        }
+    }
+};
+
+#endif
+
+//Possible states:
+//1. unopened tiles: blank and clickable (cover the board at the start of the game, can also be made by removing flags)
 //2. opened tiles: exposed and unclickable
-//2.1 numbered tiles
-//2.2 blank tiles
+//2.1 numbered tiles (can show 1-8)
+//2.2 blank tiles(no mines are on the diagonal/adjacent to the tile)
 //3. flagged tiles: unopened cells marked by the player to indicate a potential mine location, unclickable
-//4. (?) Question marked tiles
+//4. (?) Question marked tiles (appear after right-clicking a flagged tile; only exists in certain implementations)
